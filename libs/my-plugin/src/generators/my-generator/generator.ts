fa619034ed8e9b5afa9f1f7720cbd41d163bdbb3
Lara <lara@laranewsom.com>
2022-10-11 10:47:56 -0500

    added overwritten property

--- libs/my-plugin/src/generators/my-generator/generator.ts
+++ libs/my-plugin/src/generators/my-generator/generator.ts
@@ -5,6 +5,8 @@ import { MyGeneratorGeneratorSchema } from './schema';
 import { libraryGenerator } from '@nrwl/angular/generators';
 
 export default async function (tree: Tree, options: MyGeneratorGeneratorSchema) {
+  options.importPath = `@nxConfApp/${options.name}`;
+
   await libraryGenerator(tree, options);
 }
 
--- libs/my-plugin/src/generators/my-generator/schema.d.ts
+++ libs/my-plugin/src/generators/my-generator/schema.d.ts
@@ -2,4 +2,7 @@ export interface MyGeneratorGeneratorSchema {
     name: string;
     tags?: string;
     directory?: string;
+    importPath?: string;
 }
+
+
